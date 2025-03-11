f305233c5fe4adab640f72c5c12a5ec00f058e51
studentcoll <studentcoll@tp.local>
2025-03-11 09:47:32 +0300

    Исправлена кодировка файлов

--- RestorePoint.h
+++ RestorePoint.h
@@ -1,1 +1,16 @@
  
+#ifndef RESTOREPOINT_H
+#define RESTOREPOINT_H
+
+#include <vector>      
+#include "BackupObject.h" 
+
+
+class RestorePoint {
+public:
+    std::vector<BackupObject> files; 
+
+    explicit RestorePoint(const std::vector<BackupObject>& files); 
+};
+
+#endif // RESTOREPOINT_H
