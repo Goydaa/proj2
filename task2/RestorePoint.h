0660cf6127c72481c4444d8885516fcc10b2ab89
Goydaa <URNAZXC@GMAIL.COM>
2025-03-12 08:36:28 +0300

    Update RestorePoint.h

--- task2/RestorePoint.h
+++ task2/RestorePoint.h
@@ -1,16 +1,15 @@
- 
 #ifndef RESTOREPOINT_H
 #define RESTOREPOINT_H
 
-#include <vector>      
-#include "BackupObject.h" 
+#include <vector>      // Для хранения файлов в точке восстановления
+#include "BackupObject.h" // Включаем объявление класса BackupObject
 
-
+// Класс для представления точки восстановления
 class RestorePoint {
 public:
-    std::vector<BackupObject> files; 
+    std::vector<BackupObject> files; // Список файлов в точке восстановления
 
-    explicit RestorePoint(const std::vector<BackupObject>& files); 
+    explicit RestorePoint(const std::vector<BackupObject>& files); // Конструктор
 };
 
 #endif // RESTOREPOINT_H
