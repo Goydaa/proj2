ad13bd957376dc8833759554f9384ba51b1ce0e7
Goydaa <URNAZXC@GMAIL.COM>
2025-03-12 08:33:37 +0300

    Update SplitStorage.h

--- task2/SplitStorage.h
+++ task2/SplitStorage.h
@@ -1,10 +1,9 @@
- 
 #ifndef SPLITSTORAGE_H
 #define SPLITSTORAGE_H
 
-#include "IStorage.h" 
+#include "IStorage.h" // Подключаем интерфейс хранения
 
-
+// Класс для раздельного хранения файлов
 class SplitStorage : public IStorage {
 public:
     RestorePoint createRestorePoint(const std::vector<BackupObject>& files) override;
