71be93c5ca4eda11356d25c7901654eac533e5a7
Goydaa <URNAZXC@GMAIL.COM>
2025-03-12 08:33:57 +0300

    Update SplitStorage.cpp

--- task2/SplitStorage.cpp
+++ task2/SplitStorage.cpp
@@ -1,7 +1,6 @@
- 
-#include "SplitStorage.h" 
+#include "SplitStorage.h" // Подключаем заголовочный файл
 
-
+// Создает точку восстановления, сохраняя файлы раздельно
 RestorePoint SplitStorage::createRestorePoint(const std::vector<BackupObject>& files) {
-    return RestorePoint(files); 
+    return RestorePoint(files); // Просто возвращает копию списка файлов
 }
